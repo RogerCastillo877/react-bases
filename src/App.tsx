3b9112992c1c74dc91f977900d53ab35c8d991f2
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-13 21:30:28 -0500

    UseEffect, %c and Math.min

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Counter } from './bases/Counter';
 import { CounterBy } from './bases/CounterBy';
+import { CounterEffect } from './bases/CounterEffect';
 
 function App() {
   return (
     <>
-      <CounterBy />
+      <CounterEffect />
     </>
   );
 };
